35892dac2a0ef948815248554e2ee9169fa90f6e
yozaam <yozaam@gmail.com>
2021-05-10 17:45:21 +0530

    added objective carrying boolean on player character

--- Private/FPSObjectiveActor.cpp
+++ Private/FPSObjectiveActor.cpp
@@ -2,6 +2,8 @@
 
 
 #include "FPSObjectiveActor.h"
+
+#include "FPSCharacter.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -9,8 +11,7 @@
 // Sets default values
 AFPSObjectiveActor::AFPSObjectiveActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
@@ -36,10 +37,19 @@ void AFPSObjectiveActor::PlayEffects()
 	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
 }
 
-// Called every frame
-void AFPSObjectiveActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlayEffects();
+
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (MyCharacter)
+	{
+		MyCharacter->bIsCarryingObjective = true;
+
+		Destroy();
+	}
 }
 
--- Public/FPSObjectiveActor.h
+++ Public/FPSObjectiveActor.h
@@ -35,7 +35,6 @@ protected:
 	void PlayEffects();
 	
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
 };
